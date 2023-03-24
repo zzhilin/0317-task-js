fe014882c4956380cc56ed2ce4ccf43fabecc2ca
zzhilin <zl635@cornell.edu>
2023-03-24 13:45:21 -0400

    added task 1.2 implementation

---
+++ index.js
@@ -0,0 +1,26 @@
+const elements = document.querySelectorAll(".element");
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  elements.forEach((element) => {
+    if (isInViewport(element)) {
+      element.classList.add("visible");
+      console.log(element);
+      console.log("above is visible element");
+    } else {
+      element.classList.remove("visible");
+    }
+  });
+}
+
+window.addEventListener("click", handleScroll);
